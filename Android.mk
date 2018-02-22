01fc4e307cb9837f5fdbdebad3eff378a765711a
SpiritCroc <spiritcroc@gmail.com>
2018-02-22 16:59:52 +0100

    Convert to static library

--- Android.mk
+++ Android.mk
@@ -2,22 +2,16 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_MODULE := AicpGear
 
 LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_JAR_EXCLUDED_FILES := none
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_MODULE := com.aicp.gear
+LOCAL_SHARED_ANDROID_LIBRARIES := android-support-v7-preference
+LOCAL_SHARED_ANDROID_LIBRARIES += android-support-v14-preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v7-preference
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v14-preference
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-# No resources in java library
-#LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-#LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v7/preference/res
-#LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v14/preference/res
-#LOCAL_AAPT_FLAGS := --auto-add-overlay
-#LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.preference:android.support.v14.preference
-
-include $(BUILD_JAVA_LIBRARY)
+include $(BUILD_STATIC_JAVA_LIBRARY)
