b5d47999418abd553b21bf44a6091f3b34b0c82b
SpiritCroc <spiritcroc@gmail.com>
2018-02-22 14:39:25 +0100

    Initial aicpgear

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := com.aicp.gear
+
+include $(BUILD_JAVA_LIBRARY)
