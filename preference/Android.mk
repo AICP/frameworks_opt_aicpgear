4b4deb84c787ec63abde37782b18771e6f470dfd
SpiritCroc <dev@spiritcroc.de>
2018-10-26 17:27:09 +0200

    AicpGear: Add ColorBlendPreference
    Change-Id: Ia1d9575791e94f98ff66a89410e204d26cef2e40

--- preference/Android.mk
+++ preference/Android.mk
@@ -16,7 +16,8 @@ LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    android-support-v14-preference
+    android-support-v14-preference \
+    AicpGear-util \
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
