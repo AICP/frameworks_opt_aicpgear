594accf4d6e6916a20a08937fa63c24fb433a435
Michele Bono <bono.michele94@gmail.com>
2019-01-13 13:16:36 +0100

    aicpgear: import SelfRemovingPreference
    Thanks to LineageOS sdk for code
    
    Change-Id: If1fcbaccdcab4d87624be9544373a60913d3d29f

---
+++ preference/src/com/aicp/gear/preference/SelfRemovingPreferenceScreen.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.aicp.gear.preference;
+
+import android.content.Context;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceViewHolder;
+import android.support.v7.preference.R;
+import android.util.AttributeSet;
+
+/**
+ * A PreferenceScreen which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public class SelfRemovingPreferenceScreen extends PreferenceScreen {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingPreferenceScreen(Context context, AttributeSet attrs,
+                                  int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+    }
+
+    public SelfRemovingPreferenceScreen(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, 0);
+    }
+
+    public SelfRemovingPreferenceScreen(Context context, AttributeSet attrs) {
+        this(context, attrs, ConstraintsHelper.getAttr(
+                context, R.attr.preferenceStyle, android.R.attr.preferenceStyle));
+    }
+
+    public SelfRemovingPreferenceScreen(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+}
