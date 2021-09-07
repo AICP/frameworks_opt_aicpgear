b5fb45e60052e1d2a615689f63ef319d564efa6d
Ali B <abittin@gmail.com>
2021-09-07 15:11:28 +0300

    gear: add new cutouttype method
    Use cutout rectangle location to determine the cutout type
    if any.
    
    Change-Id: Ibe78363c05c5343c230f66edd38ef3041b466ef0

--- util/src/com/aicp/gear/util/DeviceUtils.java
+++ util/src/com/aicp/gear/util/DeviceUtils.java
@@ -15,12 +15,17 @@
 */
 package com.aicp.gear.util;
 
+import static android.view.DisplayCutout.BOUNDS_POSITION_LEFT;
+import static android.view.DisplayCutout.BOUNDS_POSITION_RIGHT;
+
 import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
@@ -32,6 +37,7 @@ import android.provider.Settings;
 import android.os.Vibrator;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
+import android.view.DisplayCutout;
 import android.view.DisplayInfo;
 import android.view.WindowManager;
 import android.util.Log;
@@ -43,12 +49,18 @@ import java.util.List;
 
 public class DeviceUtils {
 
+    private static final String TAG = "AicpGearDeviceUtils";
+
+    private static final boolean DEBUG = false;
+
     private static final String SETTINGS_METADATA_NAME = "com.android.settings";
 
     // Device types
     private static final int DEVICE_PHONE  = 0;
     private static final int DEVICE_HYBRID = 1;
     private static final int DEVICE_TABLET = 2;
+
+    private static final int NO_CUTOUT = -1;
 
     public static boolean deviceSupportsRemoteDisplay(Context ctx) {
         DisplayManager dm = (DisplayManager) ctx.getSystemService(Context.DISPLAY_SERVICE);
@@ -210,5 +222,32 @@ public class DeviceUtils {
             // Ignore
         }
         return false;
+    }
+
+    public static int getCutoutType(Context context) {
+        final DisplayInfo info = new DisplayInfo();
+        context.getDisplay().getDisplayInfo(info);
+        final DisplayCutout cutout = info.displayCutout;
+        if (cutout == null) {
+            if (DEBUG) Log.v(TAG, "noCutout");
+            return NO_CUTOUT;
+        }
+        final Point displaySize = new Point();
+        context.getDisplay().getRealSize(displaySize);
+        List<Rect> cutOutBounds = cutout.getBoundingRects();
+        if (cutOutBounds != null) {
+            for (Rect cutOutRect : cutOutBounds) {
+                if (DEBUG) Log.v(TAG, "cutout left= " + cutOutRect.left);
+                if (DEBUG) Log.v(TAG, "cutout right= " + cutOutRect.right);
+                if (cutOutRect.left == 0 && cutOutRect.right > 0) {  //cutout is located on top left
+                    if (DEBUG) Log.v(TAG, "cutout position= " + BOUNDS_POSITION_LEFT);
+                    return BOUNDS_POSITION_LEFT;
+                } else if (cutOutRect.right == displaySize.x && (displaySize.x - cutOutRect.left) > 0) {  //cutout is located on top right
+                    if (DEBUG) Log.v(TAG, "cutout position= " + BOUNDS_POSITION_RIGHT);
+                    return BOUNDS_POSITION_RIGHT;
+                }
+            }
+        }
+        return NO_CUTOUT;
     }
 }
